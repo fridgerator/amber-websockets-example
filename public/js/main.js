bfd95da9f6a75b15a5c9b10e60ff378653cbf4fb
Nick Franken <shnick@gmail.com>
2017-06-10 10:50:51 -0500

    updated styles

--- public/js/main.js
+++ public/js/main.js
@@ -6,16 +6,36 @@ let name = ''
 var Room = {
   template: `
     <div class="room">
-      <div>room : {{ roomName }}</div>
-
-      <div class="messages">
-        <div v-for="message in chatMessages" class="message">
-          {{ message.user }}: {{ message.message }}
+      <div class="row">
+        <div class="col s12">
+          <h4>Room : {{ roomName }}</h4>
         </div>
       </div>
 
-      <input type="text" @keyup.enter="sendMessage" v-model="message" />
-      <input type="submit" @click="sendMessage" />
+      <div class="row">
+        <div class="col s12">
+          <div class="messages">
+            <ul class="collection">
+              <li class="collection-item message" v-for="message in chatMessages">
+                {{ message.user }}: {{ message.message }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col s12">
+          <div class="input-field">
+            <input id="Message" type="text" @keyup.enter="sendMessage" v-model="message" />
+            <label for="Message">Message</label>
+          </div>
+          
+          <button @click="sendMessage()" class="btn waves-effect waves-light" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
+      </div>
     </div>
   `,
   data: function() {
@@ -26,33 +46,46 @@ var Room = {
   },
   methods: {
     onMessage (msg) {
-      console.log('new message : ', msg)
       this.chatMessages.push(msg)
+      this.chatMessages = this.chatMessages.slice(0, 100)
+      this.$nextTick(() => {
+        this.$messages.scrollTop(this.$messages.prop('scrollHeight') + 100)
+      })
     },
     sendMessage () {
       this.channel.push('msg:new', {message: this.message, user: name})
       this.message = ''
+      $('input#Message').focus()
     }
   },
   mounted: function(){
+    this.$messages = $('.messages')
     let socket = new Amber.Socket("/chat")
     socket.connect()
     .then(() => {
       this.channel = socket.channel(`chat_room:${roomName}`)
       this.channel.join()
       this.channel.on('msg:new', (msg) => this.onMessage(msg))
+      this.channel.push('msg:new', {message: 'has joined the room', user: name})
     })
+
+    $('input#Message').focus()
   }
 }
 
 var Rooms = {
   template: `
     <div class="rooms">
-      <p> enter your name and pick a room </p>
+      <h4> Enter your name and pick a room </h4>
 
-      <input type="text" v-model="name">
+      <div class="input-field">
+        <input type="text" id="name" v-model="name">
+        <label for="name">Name</label>
+      </div>
 
-      <div class="room" v-for="room in rooms" @click="joinRoom(room)">{{ room }}</div>
+      <div class="collection">
+        <a href="#!" class="collection-item room" v-for="room in rooms" @click.stop="joinRoom(room)">{{ room }}</a>
+      </div>
     </div>
   `,
   data: function() {
